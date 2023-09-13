52ac864635cd949ee347a38879eabd56f2abe5de
Harrison <Harrison@fanruan.com>
2023-09-13 15:45:42 +0800

    publish blogs

--- scripts/copy-brain.sh
+++ scripts/copy-brain.sh
@@ -2,15 +2,15 @@
 
 # 获取当前脚本的绝对路径
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-echo "[copy]script_dir is $script_dir"
+echo "[copy]current dir is $script_dir"
 
 # 构建源目录的绝对路径
 source_dir="$script_dir/../../TheBrain"
-echo "[copy]source_dir is $source_dir"
+echo "[copy]source is $source_dir"
 
 # 构建目标目录的绝对路径
 target_dir="$script_dir/../"
-echo "[copy]target_dir is $target_dir"
+echo "[copy]target is $target_dir"
 
 copy_script="$script_dir/copy.sh"
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,11 +2,12 @@
 
 # 获取当前脚本的绝对路径
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-echo "[publish]script_dir is $script_dir"
 
 # 切换到主目录
 cd "$script_dir/../"
+echo "[cd] $script_dir/../"
 
+echo "[git] backup start"
 # 添加所有修改到暂存区
 git add .
 # 检查 git 状态
@@ -14,18 +15,19 @@ status=$(git status --porcelain)
 # 如果 git 状态为空，则没有需要 commit 的内容
 if [[ -z "$status" ]]; then
   echo "no commits exit "
-  exit 0
+else
+  # 如果 git 状态不为空，则有需要 commit 的内容
+  echo "commits: "
+  echo "$status"
+  # 提交代码，使用传递的参数作为提交消息
+  git commit -m "publish blogs"
+  # 拉取最新代码
+  git pull
+  # 推送到远程仓库
+  git push  
 fi
-# 如果 git 状态不为空，则有需要 commit 的内容
-echo "commits: "
-echo "$status"
-# 提交代码，使用传递的参数作为提交消息
-git commit -m "publish blogs"
-# 拉取最新代码
-git pull
-# 推送到远程仓库
-git push
+echo "[git] backup complete"
 
 # deploy
-echo "[deploy] content will deploy to vercel"
+echo "[deploy] do vercel deploy"
 vercel
