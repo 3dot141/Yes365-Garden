c97b315bd7fccf8c62307c4043b89c6f1054e5da
Harrison <472066376@qq.com>
2024-12-28 00:46:56 +0800

    fix pr error

--- quartz/plugins/filters/excalidraw.ts
+++ quartz/plugins/filters/excalidraw.ts
@@ -3,7 +3,7 @@ import { QuartzFilterPlugin } from "../types"
 export const RemoveExcalidraws: QuartzFilterPlugin<{}> = () => ({
   name: "RemoveExcalidraws",
   shouldPublish(_ctx, [_tree, vfile]) {
-    const excalidraw: boolean = !vfile.data?.frontmatter?.tags.includes("excalidraw")
+    const excalidraw: boolean = !vfile.data?.frontmatter?.tags?.includes("excalidraw")
     return excalidraw!
   },
 })
